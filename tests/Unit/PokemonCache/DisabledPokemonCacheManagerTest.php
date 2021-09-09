d32dc6e66ba51691c2db62e98ab71bc058375177
samuele.salvatico <samuele.salvatico@onebip.com>
2021-09-09 13:40:17 +0200

    Added tests

---
+++ tests/Unit/PokemonCache/DisabledPokemonCacheManagerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Pokeball;
+
+use App\Libraries\Pokemon\BasicPokemon;
+use App\Libraries\PokemonCache\DisabledPokemonCacheManager;
+use Tests\TestCase;
+
+class DisabledPokemonCacheManagerTest extends TestCase
+{
+    /**
+     * @dataProvider methods
+     */
+    public function testValueIsNotCached(string $methodName)
+    {
+        $cacheManager = new DisabledPokemonCacheManager();
+        $firstPokemon = $cacheManager->$methodName('poke', function () {
+            return BasicPokemon::box('poke', 'mon', false, null);
+        });
+        $secondPokemon = $cacheManager->$methodName('poke', function () {
+            return BasicPokemon::box('second', 'mon', false, null);
+        });
+
+        $this->assertNotEquals($firstPokemon->name(), $secondPokemon->name());
+        $this->assertSame('second', $secondPokemon->name());
+    }
+
+    public function methods(): array
+    {
+        return [
+            ['translated'],
+            ['basic'],
+        ];
+    }
+}
---
+++ tests/Unit/PokemonCache/EnabledPokemonCacheManagerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Pokeball;
+
+use App\Libraries\Pokemon\BasicPokemon;
+use App\Libraries\PokemonCache\EnabledPokemonCacheManager;
+use Tests\TestCase;
+
+class EnabledPokemonCacheManagerTest extends TestCase
+{
+    /**
+     * @dataProvider methods
+     */
+    public function testValueIsCached(string $methodName)
+    {
+        $cacheManager = new EnabledPokemonCacheManager();
+        $firstPokemon = $cacheManager->$methodName('poke', function () {
+            return BasicPokemon::box('poke', 'mon', false, null);
+        });
+        $secondPokemon = $cacheManager->$methodName('poke', function () {
+            return BasicPokemon::box('second', 'mon', false, null);
+        });
+
+        $this->assertSame($firstPokemon->name(), $secondPokemon->name());
+        $this->assertSame('poke', $secondPokemon->name());
+    }
+
+    public function methods(): array
+    {
+        return [
+            ['translated'],
+            ['basic'],
+        ];
+    }
+}
